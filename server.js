4e6bd9fe63b43f27792a3d9ab173165e613f6328
vwls <fhbach@gmail.com>
2020-03-19 12:34:21 -0400

    added placeholder images

--- server.js
+++ server.js
@@ -4,27 +4,33 @@ const express = require('express');
 const app = express();
 
 const hostname = 'localhost';
-const port = 8080;
+const port = 8000;
 
 const bodyParser = require('body-parser');
 app.use( bodyParser.json() );
 app.use( bodyParser.urlencoded({ extended: true }) );
 
-//app.use('/public', express.static('public'));
-app.use('/views', express.static('views'));
+
+app.use('/views', express.static('views')); //this one works
+//app.use(express.static(__dirname + 'views'));
+app.use('/assets', express.static('views/assets/images'));
+
 app.set('view engine', 'ejs');
 
 app.listen(port, () => console.log(`App listening on port ${port}`));
 
 
 // Routes
-//app.get('/week6', testFunction);
 
 app.get('/index', function (req, res) {
   res.render('home');
 })
 
 app.get('/', function (req, res) {
+  res.render('home');
+})
+
+app.get('/home', function (req, res) {
   res.render('home');
 })
 
@@ -52,6 +58,10 @@ app.get('/ctc3002', function (req, res) {
   res.render('ctc3002');
 })
 
+// 404 page
+app.get('*', function(req, res){
+  res.status(404).render('404');
+});
 
 
 // function testFunction(req, res){
